da3c091767ee1618a64851c675c84acca8e77b7e
Collapsence <collapsence@github.com>
2020-05-14 18:00:34 +0300

    loading fast

--- frontend/views/layouts/_headerWhite.php
+++ frontend/views/layouts/_headerWhite.php
@@ -382,16 +382,6 @@ $my_requests = Contacts::myRequestsList();
         height: 10px!important;
     }
     }</style>
-    <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,,500,600,700" rel="stylesheet">
-    <link rel="stylesheet" href="/css/open-iconic-bootstrap.min.css">
-    <link rel="stylesheet" href="/css/animate.css">
-    <link rel="stylesheet" href="/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="/css/magnific-popup.css">
-    <link rel="stylesheet" href="/css/aos.css">
-    <link rel="stylesheet" href="/css/ionicons.min.css">
-    <link rel="stylesheet" href="/css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="/css/flaticon.css">
-    <link rel="stylesheet" href="/css/icomoon.css">
     <link rel="stylesheet" href="/css/style.css">
     <script src="/js/jquery.min.js"></script>
     <script src="/isInViewport.js"></script>
