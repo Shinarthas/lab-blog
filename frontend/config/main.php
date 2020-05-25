38b7a485af7667cdfee00357457b022606de4e10
Collapsence <collapsence@github.com>
2020-05-25 18:15:14 +0300

    back

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -7,7 +7,6 @@ $params = array_merge(
 );
 
 return [
-    '/contacts/save-client-email'=>'/contacts/save-client-email',
     'id' => 'app-frontend',
     'sourceLanguage' => 'ru',
     'language' => 'ru',
@@ -57,7 +56,7 @@ return [
                 '/services'=>'/services/index',
                 '/about'=>'/company/index',
                 '/contacts'=>'/contacts/index',
-                
+                '/contacts/save-client-email'=>'/contacts/save-client-email',
                 '/cookies'=>'/static/cookie',
                 '/terms'=>'/static/terms',
                 '/privacy'=>'/static/privacy',
